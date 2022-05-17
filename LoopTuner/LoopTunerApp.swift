65f7e1046a8a16f5216723c8edc6e1f80a918059
Randall Knutson <randall@knutson.io>
2022-05-16 20:33:10 -0500

    Initial Commit

---
+++ LoopTuner/LoopTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopTunerApp.swift
+//  LoopTuner
+//
+//  Created by Randall Knutson on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LoopTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
